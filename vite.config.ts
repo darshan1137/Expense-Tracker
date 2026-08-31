3924f8f5f4265860019f2056fab8a627a04cf058
Darshan Khapekar <darshankhapekar8520@gmail.com>
2026-08-31 20:17:54 +0530

    fix: remove unused extensions from vite pwa globPatterns to fix vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
